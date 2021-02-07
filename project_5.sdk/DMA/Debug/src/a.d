4e96412341d73debb69a6e1300bedc40ac00969e
ming <727216411@qq.com>
2021-02-07 21:02:34 +0800

    first commit

---
+++ project_5.sdk/DMA/Debug/src/a.d
@@ -0,0 +1,67 @@
+src/a.o src/a.o: ../src/a.c \
+ ../../DMA_bsp/psu_cortexa53_0/include/xaxidma.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xaxidma_bdring.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xstatus.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xil_types.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xil_assert.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xaxidma_bd.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xaxidma_hw.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xil_io.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xil_printf.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xparameters.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xparameters_ps.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/bspconfig.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xpseudo_asm.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xreg_cortexa53.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xpseudo_asm_gcc.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xdebug.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xil_cache.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xparameters.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xil_exception.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xscugic.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xscugic_hw.h \
+ ../../DMA_bsp/psu_cortexa53_0/include/xil_exception.h
+
+../../DMA_bsp/psu_cortexa53_0/include/xaxidma.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xaxidma_bdring.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xstatus.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xil_types.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xil_assert.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xaxidma_bd.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xaxidma_hw.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xil_io.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xil_printf.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xparameters.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xparameters_ps.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/bspconfig.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xpseudo_asm.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xreg_cortexa53.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xpseudo_asm_gcc.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xdebug.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xil_cache.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xparameters.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xil_exception.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xscugic.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xscugic_hw.h:
+
+../../DMA_bsp/psu_cortexa53_0/include/xil_exception.h:
